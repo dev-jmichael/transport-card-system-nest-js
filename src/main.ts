b05077e3398665d677050dfc5baaa8c816af80b4
dev-jmichael <flores.johnmichael.c.0511@gmail.com>
2024-10-09 04:18:53 +0800

    Add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionHandler } from './common/exceptions/globalexception.handler';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionHandler());
+
+  const config = new DocumentBuilder()
+    .setTitle('Transport Card API')
+    .setDescription('API for managing transport cards and transactions')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
